a4a556d62119c39614f4552622590a0187364044
Stefano Magni <stefano@creeostudio.it>
2018-07-30 19:48:00 +0200

    wrote the first test

---
+++ test/go-to-google.e2e.test.js
@@ -0,0 +1,28 @@
+/**
+ * The simplest test just to check that all the necessary dependancies are ready for the workshop
+ */
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+let browser;
+let page;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({});
+  page = await browser.newPage();
+});
+afterAll(async () => {
+  await browser.close();
+});
+
+describe(`That's our first E2E test`, () => {
+  test(`The button brings the user to google.com`, async (done) => {
+    await page.goto(`file:${path.join(__dirname, './../dist/index.html')}`);
+
+    // and then:
+    // - you should click the button
+    // - you should check if the page is been loaded
+    // - call done()
+
+  }, 10000);
+});
