f4e0ba457061fef6394126408a1c54fcbdcf19a8
Stefano Magni <stefano@creeostudio.it>
2018-07-30 19:48:35 +0200

    test-1 suitable solution

--- test/go-to-google.e2e.test.js
+++ test/go-to-google.e2e.test.js
@@ -19,10 +19,13 @@ describe(`That's our first E2E test`, () => {
   test(`The button brings the user to google.com`, async (done) => {
     await page.goto(`file:${path.join(__dirname, './../dist/index.html')}`);
 
-    // and then:
-    // - you should click the button
-    // - you should check if the page is been loaded
-    // - call done()
+    // always add a 'data-test' attribute to the elements that will parteccipate to your tests
+    await page.click('*[data-test="button"]');
 
+    // waiting for en element is a good way to be 100% sure that the page is been loaded
+    // again: use a data-test attribute in your sites
+    await page.waitForSelector('#hplogo');
+
+    done();
   }, 10000);
 });
