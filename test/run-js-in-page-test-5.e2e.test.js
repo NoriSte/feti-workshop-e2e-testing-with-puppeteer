4d220da01e6e42f01cc94ad6e05c70c42ff8e0f5
Stefano Magni <stefano@creeostudio.it>
2018-08-03 18:20:44 +0200

    wrote the ending part of the test 5

--- test/run-js-in-page-test-5.e2e.test.js
+++ test/run-js-in-page-test-5.e2e.test.js
@@ -16,29 +16,30 @@ afterAll(async () => {
 });
 
 describe(`Test 5`, () => {
+  const inputSelector = '#app form input';
+  const buttonSelector = '#app form button';
+  const itemSelector = '#app li';
+  const removeItemSelector = '#app li button';
   const todos = [
     'Subscribe to the FETI workswhop',
     'Attend the FETI workshop',
     'Pass this test',
     'Join the FETI volunteers'
   ];
-  const done = [
-    'Subscribe to the FETI workswhop',
-    'Attend the FETI workshop'
-  ];
-  const inputSelector = '#app form input';
-  const buttonSelector = '#app form button';
-  const itemSelector = '#app li';
+
+  const addTodo = async (todo) => {
+    // fill the input
+    await page.type(inputSelector, todo);
+    // click the button
+    await page.click(buttonSelector);
+  };
 
   beforeAll(async () => {
     await page.goto(`file:${path.join(__dirname, './../dist/test-5_ooade_vuex-examples_tree_simple-todo/index.html')}`);
   });
 
   test(`Add a new todo`, async () => {
-    // fill the input
-    await page.type(inputSelector, todos[0]);
-    // click the button
-    await page.click(buttonSelector);
+    await addTodo(todos[0]);
 
     // run a script in page to get the innerText of the new todo
     const innerText = await page.evaluate((selector) => {
@@ -48,5 +49,23 @@ describe(`Test 5`, () => {
 
     // and then check it with Jest
     expect(innerText.includes(todos[0])).toEqual(true);
+  }, 10000);
+
+  test(`Add all the remaining todos`, async (done) => {
+    for(let i = 1, n = todos.length; i < n; i++) {
+      await addTodo(todos[i]);
+    }
+
+    done();
+  }, 10000);
+
+  test(`Remove the first two todos`, async (done) => {
+    // it should be very easy...
+    done();
+  }, 10000);
+
+  test(`Check the remaining todos directly from the store`, async () => {
+    // @see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageevaluatepagefunction-args
+    expect(/* ... */).toEqual(['Pass this test', 'Join the FETI volunteers']);
   }, 10000);
 });
