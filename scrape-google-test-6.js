5a148fad99a7fd0abde5670f6610abb89b8f2d61
Stefano Magni <stefano@creeostudio.it>
2018-08-06 07:37:00 +0200

    test-6 suitable solution

--- scrape-google-test-6.js
+++ scrape-google-test-6.js
@@ -19,8 +19,26 @@ const puppeteer = require('puppeteer');
 
 
   const scraping = async (result = []) => {
+    const els = await page.$$(resultSelector);
 
-    // you have to read the first 30 results from Google for the given query, have fun :)
+    // URLs reading
+    for(let i = result.length, j = 0, m = els.length; i < limit && j < m; i++, j++) {
+      // you have two ways of retrieve en element attribute, one is through JSHandle functions...
+      // @see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-jshandle
+      const jsHandle = await els[j].getProperty('href');
+      const url = await jsHandle.jsonValue();
+
+      result.push(url);
+    }
+
+    // next page clicking
+    if(result.length < limit && await page.$(nextButtonSelector)) {
+      // ... and one with the already seen <code>page.evaluate</code>
+      const url = await page.evaluate((selector) => document.querySelector(selector).getAttribute('href'), nextButtonSelector);
+
+      await page.goto(domain + url);
+      result = scraping(result);
+    }
 
     return result;
   }
