a94826ed599b2e1001420a397540e42b345041c1
Stefano Magni <stefano@creeostudio.it>
2018-08-30 07:23:04 +0200

    refactoring and written a test

--- scrape-google-test-6.js
+++ scrape-google-test-6.js
@@ -5,24 +5,27 @@ Scrape the latest 40 URLs from Google searching for the "bitcoin" query
 
 const puppeteer = require('puppeteer');
 
-(async () => {
+const scrapeGoogle = async (options) => {
+  const defaultOptions = {
+    domain: 'https://www.google.it',
+    limit: 30,
+    query: 'bitcoin'
+  };
+  options = {...defaultOptions, ...options};
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
 
-  const limit = 30;
-  const domain = 'https://www.google.it';
-  const query = 'bitcoin';
   const resultSelector = 'div.g h3.r a';
   const nextButtonSelector = '#pnnext';
 
-  await page.goto(`${domain}/?gfe_rd=cr&ei=J4KAWL2qBovCXpDYgRg#q=${query}`);
+  await page.goto(`${options.domain}/?gfe_rd=cr&ei=J4KAWL2qBovCXpDYgRg#q=${options.query}`);
 
 
   const scraping = async (result = []) => {
     const els = await page.$$(resultSelector);
 
     // URLs reading
-    for(let i = result.length, j = 0, m = els.length; i < limit && j < m; i++, j++) {
+    for(let i = result.length, j = 0, m = els.length; i < options.limit && j < m; i++, j++) {
       // you have two ways of retrieve en element attribute, one is through JSHandle functions...
       // @see https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#class-jshandle
       const jsHandle = await els[j].getProperty('href');
@@ -32,11 +35,11 @@ const puppeteer = require('puppeteer');
     }
 
     // next page clicking
-    if(result.length < limit && await page.$(nextButtonSelector)) {
+    if(result.length < options.limit && await page.$(nextButtonSelector)) {
       // ... and one with the already seen <code>page.evaluate</code>
       const url = await page.evaluate((selector) => document.querySelector(selector).getAttribute('href'), nextButtonSelector);
 
-      await page.goto(domain + url);
+      await page.goto(options.domain + url);
       result = scraping(result);
     }
 
@@ -44,7 +47,8 @@ const puppeteer = require('puppeteer');
   }
 
   const result = await scraping();
-  console.log(result);
   browser.close();
-  process.exit();
-})();
+  return result;
+}
+
+module.exports = scrapeGoogle;
