631e89d8d4aca125184133f5d500b4be104ef9fb
Stefano Magni <stefano@creeostudio.it>
2018-09-27 17:34:47 +0200

    moving to jest-puppeteer for the expectations

--- test/min.e2e.test.js
+++ test/min.e2e.test.js
@@ -15,8 +15,8 @@ afterAll(async () => {
 });
 
 describe('Generic check', () => {
-  test(`You're ready for the workshop :)`, async (done) => {
+  test(`You're ready for the workshop :)`, async () => {
     await page.goto('http://www.frontenders.ch/');
-    done();
+    await expect(page).toMatch('FrontEnders Ticino');
   }, 10000);
 });
