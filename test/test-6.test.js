309b50c972249cab1a1b6f2de6de995d107f2914
Stefano Magni <nori.ste.magni@gmail.com>
2018-10-11 07:20:39 +0200

    skipped some tests by default

--- test/test-6.test.js
+++ test/test-6.test.js
@@ -10,17 +10,17 @@ describe(`Test 6`, () => {
     expect(result.filter(el => el.startsWith('http')).length).toBe(30);
   }, 20000);
 
-  test(`It works with queries with 0 results`, async () => {
+  test.skip(`It works with queries with 0 results`, async () => {
     const result = await scrapeGoogle({query: 'ksjdfhklsfjhalskflaskdfaksjfkalsjfkasjfkjsafhjka'});
     expect(result.length).toBe(0);
   }, 20000);
 
-  test(`It works with queries with less than 10 results`, async () => {
+  test.skip(`It works with queries with less than 10 results`, async () => {
     const result = await scrapeGoogle({query: 'site:conio.com multisig'});
     expect(result.length).toBeLessThanOrEqual(10);
   }, 20000);
 
-  test(`It works with queries with more a number of results included between 10 and 30`, async () => {
+  test.skip(`It works with queries with more a number of results included between 10 and 30`, async () => {
     const result = await scrapeGoogle({query: 'site:conio.com wallet'});
     expect(result.length).toBeGreaterThanOrEqual(10);
     expect(result.length).toBeLessThan(30);
